7a8e7841fb507d536b6ec525712853ee1b10fa5c
Agung Kurniawan <agung.kurniawan@sehatq.com>
2023-01-24 13:58:09 +0700

    add build

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [
         laravel({
-          input: [
-            'resources/js/app.css',
-            'resources/js/app.jsx',
-          ],
-          refresh: true,
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.jsx',
+            ],
+            refresh: true,
         }),
         react(),
     ],
